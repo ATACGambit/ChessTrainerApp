0a64c263d795ff08c0bc5b2c2d1a70bb357673e3
Владимир Дурицын <vladimirduricyn@MacBook-Pro-Vladimir.local>
2025-08-20 21:27:58 +0700

    refactor: wrap ClassView content in ClassContainer

--- ChessTrainerApp/ClassView.swift
+++ ChessTrainerApp/ClassView.swift
@@ -2,60 +2,63 @@ import SwiftUI
 
 struct ClassView: View {
     var body: some View {
-        GeometryReader { geometry in
-            // Размеры экрана
-            let a = geometry.size.width
-            let b = geometry.size.height
-
-            // Параметры (можно менять для теста)
-            let c: CGFloat = 40   // толщина рамки
-            let d: CGFloat = 10   // сдвиг красной зоны от левого края
-            let w: CGFloat = 20   // ширина красной зоны
-            let e: CGFloat = 10   // отступ сверху/снизу у красной зоны
-
-            ZStack {
-                // --- Черная зона (S2) ---
-                Rectangle()
-                    .fill(Color.black)
-                    .frame(width: a - 2*c, height: b - 2*c)
-                    .position(x: a/2, y: b/2)
-
-                // --- Красная зона (S3) ---
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: w, height: b - 2*(c+e))
-                    .position(
-                        x: d + w/2,
-                        y: b/2
-                    )
-
-                // --- Серая рамка (S1 = экран - S2 - S3) ---
-                // Рисуем поверх, полупрозрачная
-                Rectangle()
-                    .fill(Color.gray.opacity(0.5))
-                    .frame(width: a, height: b)
-                    .mask(
-                        // Вырезаем черную и красную зоны
-                        ZStack {
-                            Rectangle().frame(width: a, height: b) // всё
-                            Rectangle()
-                                .frame(width: a - 2*c, height: b - 2*c)
-                                .position(x: a/2, y: b/2)
-                                .blendMode(.destinationOut)
-                            Rectangle()
-                                .frame(width: w, height: b - 2*(c+e))
-                                .position(x: d + w/2, y: b/2)
-                                .blendMode(.destinationOut)
-                        }
-                        .compositingGroup()
-                    )
+        ClassContainer {
+            GeometryReader { geometry in
+                
+                // Размеры экрана
+                let a = geometry.size.width
+                let b = geometry.size.height
+                
+                // Параметры (можно менять для теста)
+                let c: CGFloat = 40   // толщина рамки
+                let d: CGFloat = 10   // сдвиг красной зоны от левого края
+                let w: CGFloat = 20   // ширина красной зоны
+                let e: CGFloat = 10   // отступ сверху/снизу у красной зоны
+                
+                ZStack {
+                    // --- Черная зона (S2) ---
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: a - 2*c, height: b - 2*c)
+                        .position(x: a/2, y: b/2)
+                    
+                    // --- Красная зона (S3) ---
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: w, height: b - 2*(c+e))
+                        .position(
+                            x: d + w/2,
+                            y: b/2
+                        )
+                    
+                    // --- Серая рамка (S1 = экран - S2 - S3) ---
+                    // Рисуем поверх, полупрозрачная
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.5))
+                        .frame(width: a, height: b)
+                        .mask(
+                            // Вырезаем черную и красную зоны
+                            ZStack {
+                                Rectangle().frame(width: a, height: b) // всё
+                                Rectangle()
+                                    .frame(width: a - 2*c, height: b - 2*c)
+                                    .position(x: a/2, y: b/2)
+                                    .blendMode(.destinationOut)
+                                Rectangle()
+                                    .frame(width: w, height: b - 2*(c+e))
+                                    .position(x: d + w/2, y: b/2)
+                                    .blendMode(.destinationOut)
+                            }
+                                .compositingGroup()
+                        )
+                }
             }
         }
     }
-}
-
-struct ClassView_Previews: PreviewProvider {
-    static var previews: some View {
-        ClassView()
+    
+    struct ClassView_Previews: PreviewProvider {
+        static var previews: some View {
+            ClassView()
+        }
     }
 }
